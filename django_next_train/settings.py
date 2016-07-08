a10a201597cd4dbd7e158a011cd3165449b086e1
bitpixdigital <angel@bitpixdigital>
2016-07-07 22:30:17 -0400

    fixed merge conflicts

--- django_next_train/settings.py
+++ django_next_train/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'young-lake-29812.herokuapp.com', 'nexttrain.bitpixdigital.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'young-lake-29812.herokuapp.com', 'www.nexttrain.us']
 
 # Email Settings
 EMAIL_HOST = 'smtp.gmail.com'

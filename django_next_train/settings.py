e3470e335f048b80561d82ae70590c56131999a2
bitpixdigital <angel@bitpixdigital>
2016-07-07 22:25:29 -0400

    fixed merge conflicts

--- django_next_train/settings.py
+++ django_next_train/settings.py
@@ -37,7 +37,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_SUBJECT_PREFIX = 'Next Train'
-<<<<<<< HEAD
+
 # Secure email settings within included local_settings
 
 

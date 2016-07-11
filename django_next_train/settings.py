2082c3147b2c70d942ba45bb7ccbdd7656a63ddd
bitpixdigital <angel@bitpixdigital>
2016-07-11 17:59:11 -0400

    css changes

--- django_next_train/settings.py
+++ django_next_train/settings.py
@@ -141,6 +141,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
+
 
 # Settings for django-bootstrap3
 BOOTSTRAP3 = {
@@ -162,7 +167,7 @@ if os.getcwd() == '/app':
     DEBUG = False
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = 'static'
     STATICFILES_DIR = (
         os.path.join(BASE_DIR, 'static')
     )

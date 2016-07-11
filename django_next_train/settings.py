bc27eebac852afa34336f2fdec02def562d15a8c
bitpixdigital <angel@bitpixdigital>
2016-07-11 18:59:08 -0400

    css changes

--- django_next_train/settings.py
+++ django_next_train/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'young-lake-29812.herokuapp.com', 'www.nexttrain.us']
 
